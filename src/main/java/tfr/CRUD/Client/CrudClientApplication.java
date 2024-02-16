9742eb7173f19e98a50bfc51104773f46f36d68f
thiagorabaioli <tfrabaioli@outlook.com>
2024-02-16 21:41:36 +0000

    CRUD Client

---
+++ src/main/java/tfr/CRUD/Client/CrudClientApplication.java
@@ -0,0 +1,13 @@
+package tfr.CRUD.Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tfr/CRUD/Client/CrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tfr.CRUD.Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
